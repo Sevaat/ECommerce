ee0847e150d237b051252ac9f867ba9845999fdc
Vsevolod Tkachenko <sevaatmail@gmail.com>
2025-08-29 00:18:04 +0500

    Защитить атрибуты класса Category
    добавление продукта в список продуктов категории, ограничение доступа к списку продуктов, вывод списка продуктов, тесты.

--- src/category.py
+++ src/category.py
@@ -1,15 +1,42 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     category_count: int = 0
     product_count: int = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products: list):
         if not (isinstance(name, str) and isinstance(description, str) and isinstance(products, list)):
             raise ValueError("Недопустимое значение: неверный тип данных")
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count = len(products)
+        Category.product_count += len(products)
+
+    def add_product(self, product: Product) -> None:
+        """
+        Метод добавления продукта в список
+        :param product: продукт
+        :return:
+        """
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise ValueError("Недопустимое значение: неверный тип данных")
+
+    @property
+    def products(self) -> list:
+        return self.__products
+
+    def output_product_list(self) -> str:
+        """
+        Метод вывода списка продуктов
+        :return: список продуктов в виде текста
+        """
+        text = [f"{prd.name}, {prd.price} руб. Остаток: {prd.quantity} шт." for prd in self.__products]
+        return "\n".join(text)
