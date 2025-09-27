d4b7301d83b98b9b53e2d63b437109d8078cf450
vsevolod tkachenko <sevaatmail@gmail.com>
2025-09-27 21:06:53 +0500

    Подсчитывать средний ценник всех товаров
    В классе Category реализован новый метод, который подсчитывает средний ценник всех товаров. С помощью исключений обрабатывает случай, когда в категории нет товаров и сумма всех товаров будет делиться на ноль. Если такое происходит, возвращается ноль. Тесты.

--- src/category.py
+++ src/category.py
@@ -2,24 +2,25 @@ from src.product.product import Product
 
 
 class Category:
-    name: str
-    description: str
-    __products: list
     category_count: int = 0
     product_count: int = 0
 
     def __init__(self, name: str, description: str, products: list):
         if not (isinstance(name, str) and isinstance(description, str) and isinstance(products, list)):
             raise TypeError("Недопустимое значение: неверный тип данных")
-        self.name = name
-        self.description = description
-        self.__products = products
-        Category.category_count += 1
-        Category.product_count += len(products)
+        self.name = name  # имя категории
+        self.description = description  # описание категории
+        self.__products = products  # продукты в категории
+        Category.category_count += 1  # общее количество категорий
+        Category.product_count += len(products)  # общее количество видов товаров
 
     def __str__(self) -> str:
         quantity_of_products = sum([prd.quantity for prd in self.__products])
         return f"{self.name}, количество продуктов: {quantity_of_products} шт."
+
+    @property
+    def products(self) -> list:
+        return self.__products
 
     def add_product(self, product: Product) -> None:
         """
@@ -33,9 +34,17 @@ class Category:
         else:
             raise TypeError("Недопустимое значение: неверный тип данных")
 
-    @property
-    def products(self) -> list:
-        return self.__products
+    def middle_price(self) -> float:
+        """
+        Подсчет среднего ценника всех товаров в категории
+        :return: средняя цена товаров в категории
+        """
+        try:
+            price = sum([product.price * product.quantity for product in self.__products])
+            quantity = sum([product.quantity for product in self.__products])
+            return float(price / quantity)
+        except ZeroDivisionError:
+            return 0
 
     def output_product_list(self) -> str:
         """
